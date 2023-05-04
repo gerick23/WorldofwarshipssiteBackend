b859cc4012ede0799162a2712ee7815148e66e04
Niall McCarter <christopherdoherty14@gmail.com>
2023-05-05 00:20:29 +0100

    added additional user checks when

--- controllers/users.js
+++ controllers/users.js
@@ -3,6 +3,9 @@ const hasher = require('../util/hasher')
 const  User  = require('../models/users')
 const { hash } = require('bcrypt')
 const { Info } = require('../models')
+const  { getPlayerNames }  = require('../helperfunctions/api_queries')
+const { default: axios } = require('axios')
+const playerhurl = `https://api.worldofwarships.eu/wows/account/info/?application_id=${process.env.API}&account_id`;
 
 router.get('/',async(req,res)=>{
     const users = await User.findAll({
@@ -16,14 +19,13 @@ router.get('/',async(req,res)=>{
 
 
 router.post('/',async(req,res)=>{
-    console.log("req body",req.body)
     const hashedpass = hasher(req.body.hashedpassword)
-    console.log("hashedpass",hashedpass)
+    const attempt =await getPlayerNames(req.body.username);
     try{
-        const usernamesearch = await User.findOne({where:{username:req.body.username,hashedpassword:hashedpass}})
-        console.log(usernamesearch)
-        if(usernamesearch){
-            return res.status(400).json('Invalid Username or password')
+        const usernamesearch = await User.findOne({where:{username:req.body.username}})
+        if(usernamesearch || attempt.data.meta.count!=1){
+            console.log("Wtf")
+            return res.status(400).json('Invalid Username')
         }
         const user = await User.create({username:req.body.username,hashedpassword:hashedpass})
         res.json(user)
@@ -42,3 +44,5 @@ router.get('/:id',async (req,res)=>{
 })
 
 module.exports = router
+
+
---
+++ helperfunctions/api_queries.js
@@ -0,0 +1,26 @@
+const axios = require('axios')
+require('dotenv').config()
+
+const getPlayerData=async (username) => {
+    try{
+        const playerhurl = `https://api.worldofwarships.eu/wows/account/info/?application_id=${process.env.API}&account_id`;
+        const playerdata =await axios.get(playerhurl+'='+`${username}`);
+        return playerdata;
+    }catch(error){
+        console.log('Error');
+        return [];
+    }
+};
+
+const getPlayerNames=async (query) => {
+    try{
+        const searchurl = `https://api.worldofwarships.eu/wows/account/list/?application_id=${process.env.API}&search`;
+        const accounts = await axios.get(searchurl+'='+`${query}`);
+        console.log(accounts)
+        return accounts;
+    }catch(error){
+        console.log('Error');
+        return [];
+    }
+};
+module.exports = {getPlayerData,getPlayerNames}
